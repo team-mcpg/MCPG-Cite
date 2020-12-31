4c42ff3e3e013e74f3b2c318a9f8423dcc6069a0
tutur1004 <arthur.barthelme78@gmail.com>
2020-12-31 12:37:29 +0100

    Initial commit

---
+++ src/main/java/fr/milekat/cite/MainCite.java
@@ -0,0 +1,15 @@
+package fr.milekat.cite;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MainCite extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
